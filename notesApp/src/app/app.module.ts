f4b64cf8db796455466d52d872e043e22939453b
JyotiPrakash <jyothiprakash.d@terralogic.com>
2020-08-19 16:32:48 +0530

    first commit with all functionalities

---
+++ notesApp/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GlobalService } from '../app/global.service';
+import { OperationsService } from '../app/operations.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material'  
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatAutocompleteModule, MatInputModule,MatFormFieldModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatAutocompleteModule, MatInputModule,MatFormFieldModule,
+    MatExpansionModule
+  ],
+  providers: [GlobalService, OperationsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ notesApp/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatTableModule } from '@angular/material';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataSource } from '@angular/cdk/table';
+import { MatSort } from '@angular/material/sort';
+import { GlobalService } from '../global.service';
+
+export class notesDetails {
+  title: string;
+  timestamp: string;
+  id: string;
+  text: string;
+}
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  dataSource: MatTableDataSource<notesDetails>;
+  notesObject = {
+    id: "",
+    title: "",
+    text: "",
+    timestamp: "",
+    editable: false
+  }
+  isEditableCheck: boolean = true;
+  formVisibility: boolean = false;
+  summaryVisibility: boolean = false;
+  displayedColumns: string[] = ['title', 'timestamp'];
+  latestUpdatedNote: object = {};
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  constructor(public global: GlobalService) { }
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource<notesDetails>([]);
+    this.dataSource = new MatTableDataSource<notesDetails>(this.global.notesArr);
+    this.dataSource.sort = this.sort;
+
+
+
+  }
+
+  openSummary() {
+    this.latestUpdatedNote = this.global.notesArr.reduce((a, b) => {
+      return new Date(a.timestamp) > new Date(b.timestamp) ? a : b;
+    })
+    console.log(this.latestUpdatedNote);
+    this.summaryVisibility = !this.summaryVisibility
+  }
+
+  selectNote(row: object) {
+    console.log(row);
+    this.notesObject.id = row['id'];
+    this.notesObject.title = row['title'];
+    this.notesObject.text = row['text'];
+    this.notesObject.editable = row['editable'];
+    this.isEditableCheck = row['editable'];
+    this.formVisibility = true;
+  }
+
+  refreshForm() {
+    this.notesObject = {
+      id: "",
+      title: "",
+      text: "",
+      timestamp: "",
+      editable: false
+    }
+  }
+
+  saveNote() {
+
+    if (this.notesObject.title != "" && this.notesObject.text != "") {
+      if (this.notesObject.id != null && this.notesObject.id != "") { //edit existing
+        let index = this.global.notesArr.findIndex(o => o.id === this.notesObject.id);
+        console.log(index)
+        this.notesObject.timestamp = String(new Date().getTime());
+        this.global.notesArr[index] = this.notesObject;
+      } else { //add new 
+        this.notesObject.timestamp = String(new Date().getTime());
+        this.notesObject.id = String(new Date().getTime()).slice(-8);
+        this.global.notesArr.push(this.notesObject);
+      }
+
+
+      this.notesObject = {
+        id: "",
+        title: "",
+        text: "",
+        timestamp: "",
+        editable: false
+      }
+      this.dataSource = new MatTableDataSource<notesDetails>(this.global.notesArr);
+      this.dataSource.sort = this.sort;
+      this.formVisibility = false;
+    }else{
+      console.log("validation error");
+    }
+
+
+
+  }
+
+}
